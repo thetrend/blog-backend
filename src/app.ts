1983615d34ef9819705af12cae9d141315d99697
Grace de la Mora <thetrend@users.noreply.github.com>
2023-07-03 22:28:15 -0400

    chore: add Prisma and Redis

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,41 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
+
+import redisClient from './utils/redis';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const prisma = new PrismaClient();
 
-app.use(express.json());
+const bootstrap = async () => {
+  app.use(express.json());
+  
+  const port = process.env.PORT || 8000;
 
-app.get('/api/hello-world', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({
-    message: 'Hello World'
+  app.get('/api/health', async (_, res: Response) => {
+    const message = await redisClient.get('try');
+    res.status(200).json({
+      message,
+    });
   });
-});
 
-app.listen(port, () => {
-  console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
-});
+  app.get('/api/hello-world', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+      message: 'Hello World'
+    });
+  });
+  
+  app.listen(port, () => {
+    console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
+  });
+};
+
+bootstrap()
+  .catch((error) => {
+    throw error;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
