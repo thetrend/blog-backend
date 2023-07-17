f3fb1380de1a5130c9d6ab53a25548d5c7cefebd
Grace de la Mora <thetrend@users.noreply.github.com>
2023-07-16 22:22:52 -0400

    feat: create update user API endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import { getMeHandler } from '../controllers/user';
 import { deserializeUser } from '../middleware/deserializeUser';
 import { requireUser } from '../middleware/requireUser';
+import { updateUserHandler } from '../controllers/auth';
 
 const router = express.Router();
 
-router.use(deserializeUser, requireUser);
-
-router.get('/me', getMeHandler);
+router.get('/me', deserializeUser, requireUser, getMeHandler);
+router.patch('/edit/:id', deserializeUser, requireUser, updateUserHandler);
 
 export default router;
